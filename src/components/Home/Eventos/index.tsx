a28ad25eaa5ad24835bb5501cd67a048ff2ffa5d
erosMariano <erosmariano1@gmail.com>
2023-04-05 13:07:55 -0300

    feat: create hero

---
+++ src/components/Home/Eventos/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Eventos() {
+  return (
+    <div className='container'>
+      <h2>Nossos eventos</h2>
+
+      <div className="card">
+        <img src="" alt="" />
+        <h3>Eu Sou - Musical de Páscoa</h3>
+        <span>Teatro Municipal Glória Giglio</span>
+        <span>Valor: Free</span>
+        <span>Organizador</span>
+        <span>inscritos</span>
+      </div>
+    </div>
+  )
+}
+
+export default Eventos
+
+export const EventosContaine = styled.section`
+
+`
